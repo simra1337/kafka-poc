7efcf01b0b3c2a080cb9a17160c11d51ffaf3be2
Mohd Simra <mohd.simra@tothenew.com>
2023-09-29 14:52:31 +0530

    comment removed

--- producer/src/main/java/com/kafka/poc/producer/config/KafkaProducerConfig.java
+++ producer/src/main/java/com/kafka/poc/producer/config/KafkaProducerConfig.java
@@ -25,7 +25,7 @@ public class KafkaProducerConfig {
     Map<String, Object> config = new HashMap<>();
     config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
     config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class); // Use appropriate serializer
+    config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
     return new DefaultKafkaProducerFactory<>(config);
   }
